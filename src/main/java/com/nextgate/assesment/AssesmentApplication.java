a3f34edc407f57a258fbf52ac51911b210b8d457
Andrew Scott <andrew.scott@nextgate.com>
2020-10-20 15:27:31 +0100

    Initial commit for Nextgate Assignment

---
+++ src/main/java/com/nextgate/assesment/AssesmentApplication.java
@@ -0,0 +1,18 @@
+package com.nextgate.assesment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * The entry point for our Spring Boot application
+ * 
+ * @author nextgate.employee
+ */
+@SpringBootApplication
+public class AssesmentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AssesmentApplication.class, args);
+    }
+
+}
