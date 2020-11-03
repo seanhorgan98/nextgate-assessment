11de8908cc30012d7e440c1931126839b8474d86
Sean Horgan <seanhorgan98@gmail.com>
2020-11-03 20:32:26 +0000

    Added initial end points for albums and singers

--- src/main/java/com/nextgate/assesment/rest/AlbumController.java
+++ src/main/java/com/nextgate/assesment/rest/AlbumController.java
@@ -1,8 +1,34 @@
 package com.nextgate.assesment.rest;
 
+import com.nextgate.assesment.datatypes.Album;
+import com.nextgate.assesment.repositories.AlbumRepository;
+import com.nextgate.assesment.repositories.SingerRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class AlbumController {
+
+    @Autowired
+    private AlbumRepository albumRepository;
+
+    @Autowired
+    private SingerRepository singerRepository;
+
+    /**
+     * GET request for retrieving albums by singer id
+     * @param singerId
+     * @param pageable
+     * @return Page<Album>
+     */
+    @GetMapping("/singers/{singerId}/albums")
+    public Page<Album> getAllAlbumBySingerId(@PathVariable (value = "singerId") Long singerId, Pageable pageable){
+        return albumRepository.findBySingerId(singerId, pageable);
+    }
     
 }
--- src/main/java/com/nextgate/assesment/rest/SingerController.java
+++ src/main/java/com/nextgate/assesment/rest/SingerController.java
@@ -1,8 +1,43 @@
 package com.nextgate.assesment.rest;
 
+import com.nextgate.assesment.datatypes.Singer;
+import com.nextgate.assesment.repositories.SingerRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class SingerController {
     
+    @Autowired
+    private SingerRepository singerRepository;
+
+
+    /**
+     * GET method to retrieve all singers in the db
+     * @param pageable
+     * @return Page<Singer>
+     */
+    @GetMapping("/singers")
+    public Page<Singer> getAllSingers(Pageable pageable){
+        return singerRepository.findAll(pageable);
+    }
+
+    /**
+     * POST method to create a new singer
+     * @param singer
+     * @return
+     */
+    @PostMapping("/singers")
+    public Singer createSinger(@RequestBody Singer singer){
+        return singerRepository.save(singer);
+    }
+
+
 }
