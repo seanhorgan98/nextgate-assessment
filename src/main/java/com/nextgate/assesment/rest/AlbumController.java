7a69a7c0b0b0a2bf4c44cc47796c3d5b0d7d089c
Sean Horgan <seanhorgan98@gmail.com>
2020-11-05 17:52:46 +0000

    Added Get and Post mappings for albums

--- src/main/java/com/nextgate/assesment/rest/AlbumController.java
+++ src/main/java/com/nextgate/assesment/rest/AlbumController.java
@@ -1,16 +1,15 @@
 package com.nextgate.assesment.rest;
 
 import com.nextgate.assesment.datatypes.Album;
+import com.nextgate.assesment.datatypes.Singer;
+import com.nextgate.assesment.exception.ResourceNotFoundException;
 import com.nextgate.assesment.repositories.AlbumRepository;
 import com.nextgate.assesment.repositories.SingerRepository;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
-
+import org.springframework.web.bind.annotation.*;
 @RestController
 public class AlbumController {
 
@@ -21,6 +20,17 @@ public class AlbumController {
     private SingerRepository singerRepository;
 
     /**
+     * GET method to retrieve all albums in the db
+     * @param pageable
+     * @return Page<Album>
+     */
+    @GetMapping("/albums")
+    public Page<Album> getAllAlbums(Pageable pageable){
+        return albumRepository.findAll(pageable);
+    }
+
+
+    /**
      * GET request for retrieving albums by singer id
      * @param singerId
      * @param pageable
@@ -29,6 +39,21 @@ public class AlbumController {
     @GetMapping("/singers/{singerId}/albums")
     public Page<Album> getAllAlbumBySingerId(@PathVariable (value = "singerId") Long singerId, Pageable pageable){
         return albumRepository.findBySingerId(singerId, pageable);
+    }
+
+    /**
+     * POST method for creating a new album for a specific singer
+     * @param singerId
+     * @param album
+     * @return
+     */
+    @PostMapping("/singers/{singerId}/albums")
+    public Album createAlbum(@PathVariable (value = "singerId") Long singerId,
+                                 @RequestBody Album album) {
+        return singerRepository.findById(singerId).map(singer -> {
+            album.setSinger(singer);
+            return albumRepository.save(album);
+        }).orElseThrow(() -> new ResourceNotFoundException("SingerId " + singerId + " not found"));
     }
     
 }
