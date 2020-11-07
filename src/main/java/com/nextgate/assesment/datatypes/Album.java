57d82eff0d02fad52ce87a755c396d701783665f
Sean Horgan <seanhorgan98@gmail.com>
2020-11-07 16:32:09 +0000

    Updated imports

--- src/main/java/com/nextgate/assesment/datatypes/Album.java
+++ src/main/java/com/nextgate/assesment/datatypes/Album.java
@@ -1,14 +1,11 @@
 package com.nextgate.assesment.datatypes;
 
 import javax.persistence.Entity;
-import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
-
-import com.fasterxml.jackson.annotation.JsonIgnore;
 
 import org.hibernate.annotations.OnDelete;
 import org.hibernate.annotations.OnDeleteAction;
